7bc76046a73d32dd8f7f807ee56be28367851268
Alex <alex.averill2013@gmail.com>
2019-09-08 15:09:08 -0600

    Initial commit for a simple typescript constellation animation

---
+++ main.js
@@ -0,0 +1,18 @@
+var Vector2 = /** @class */ (function () {
+    function Vector2(x, y) {
+        this.x = 0;
+        this.y = 0;
+    }
+    return Vector2;
+}());
+var AnimatedPoint = /** @class */ (function () {
+    function AnimatedPoint(width, height) {
+        this.width = 0;
+        this.height = 0;
+        this.width = width;
+        this.height = height;
+    }
+    return AnimatedPoint;
+}());
+function main() {
+}
---
+++ main.ts
@@ -0,0 +1,22 @@
+class Vector2{
+    x: number = 0;
+    y : number = 0;
+    constructor(x:number, y:number){
+
+    }
+}
+
+class AnimatedPoint{
+    width = 0;
+    height = 0;
+    position : Vector2;
+    constructor(width:number , height:number){
+        this.width = width;
+        this.height = height;
+    }
+}
+
+
+function main(){
+
+}
