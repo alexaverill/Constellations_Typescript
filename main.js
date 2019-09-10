b8d7118979f15263fee705cd362f655002144dd5
Alex <alex.averill2013@gmail.com>
2019-09-09 18:55:10 -0600

    Implement constellation interactions. 1. Adding in distance functions 2. Adding in line connections
    TODO:
    1. Add some styling
    2. Add in user interface to change parameters

--- main.js
+++ main.js
@@ -2,17 +2,98 @@ var Vector2 = /** @class */ (function () {
     function Vector2(x, y) {
         this.x = 0;
         this.y = 0;
+        this.x = x;
+        this.y = y;
     }
+    Vector2.prototype.distanceTo = function (other) {
+        var xVal = other.x - this.x;
+        xVal *= xVal;
+        var yVal = other.y - this.y;
+        yVal *= yVal;
+        return Math.sqrt(xVal + yVal);
+    };
     return Vector2;
 }());
 var AnimatedPoint = /** @class */ (function () {
-    function AnimatedPoint(width, height) {
+    function AnimatedPoint(width, height, position, bounds) {
         this.width = 0;
         this.height = 0;
         this.width = width;
         this.height = height;
+        this.position = position;
+        if (Math.random() * 10 > 5) {
+            this.speed = new Vector2(1, 1);
+        }
+        else {
+            this.speed = new Vector2(1, -1);
+        }
+        this.bounds = bounds;
     }
+    AnimatedPoint.prototype.update = function () {
+        if (this.position.x > this.bounds.x) {
+            this.position.x = 10;
+        }
+        else if (this.position.x < 0) {
+            this.position.x = this.bounds.x - 10;
+        }
+        if (this.position.y > this.bounds.y) {
+            this.position.y = 10;
+        }
+        else if (this.position.y < 0) {
+            this.position.y = this.bounds.y - 10;
+        }
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+    };
+    AnimatedPoint.prototype.distanceTo = function (other) {
+        return this.position.distanceTo(other.position);
+    };
     return AnimatedPoint;
 }());
-function main() {
+var Rendering = /** @class */ (function () {
+    function Rendering(canvas) {
+        this.colorString = 'rgb(200,0,0)';
+        this.pointsList = [];
+        this.seperationDistance = 90;
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+        this.setBackgroundColor(this.colorString);
+        this.context.fillRect(0, 0, 800, 800);
+        this.isRed = true;
+        var bounds = new Vector2(this.canvas.width, this.canvas.height);
+        for (var x = 0; x < 100; x++) {
+            var xPos = Math.random() * this.canvas.width + 8;
+            var yPos = Math.random() * this.canvas.height + 8;
+            this.pointsList[x] = new AnimatedPoint(4, 4, new Vector2(xPos, yPos), bounds);
+        }
+    }
+    Rendering.prototype.setBackgroundColor = function (color) {
+        this.context.fillStyle = color;
+        this.context.fillRect(0, 0, 800, 800);
+    };
+    Rendering.prototype.update = function () {
+        this.context.fillStyle = 'black';
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (var i = 0; i < this.pointsList.length - 1; i++) {
+            this.pointsList[i].update();
+            this.context.beginPath();
+            this.context.moveTo(this.pointsList[i].position.x, this.pointsList[i].position.y);
+            this.context.arc(this.pointsList[i].position.x, this.pointsList[i].position.y, this.pointsList[i].width, 0, 360);
+            this.context.fill();
+            this.context.fill();
+            for (var x = i + 1; x < this.pointsList.length - 1; x++) {
+                if (x != this.pointsList.length - 1) {
+                    if (this.pointsList[i].distanceTo(this.pointsList[x]) <= this.seperationDistance) {
+                        this.context.lineTo(this.pointsList[x].position.x, this.pointsList[x].position.y);
+                    }
+                }
+            }
+            this.context.stroke();
+        }
+    };
+    return Rendering;
+}());
+function initialize() {
+    var render = new Rendering(document.getElementById("mainCanvas"));
+    var timer = setInterval(function () { return render.update(); }, 18);
 }
--- main.ts
+++ main.ts
@@ -1,22 +1,104 @@
-class Vector2{
+class Vector2 {
     x: number = 0;
-    y : number = 0;
-    constructor(x:number, y:number){
-
+    y: number = 0;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    distanceTo(other: Vector2){
+        let xVal: number = other.x - this.x;
+        xVal *= xVal;
+        let yVal: number = other.y - this.y;
+        yVal *= yVal;
+        return Math.sqrt(xVal +yVal);
     }
 }
 
-class AnimatedPoint{
+class AnimatedPoint {
     width = 0;
     height = 0;
-    position : Vector2;
-    constructor(width:number , height:number){
+    position: Vector2;
+    speed: Vector2;
+    bounds: Vector2;
+    constructor(width: number, height: number, position:Vector2, bounds:Vector2) {
         this.width = width;
         this.height = height;
+        this.position = position;
+        if(Math.random()* 10 > 5){
+            this.speed = new Vector2(1,1);
+        }else{
+            this.speed = new Vector2(1,-1);
+        }
+        
+        this.bounds = bounds;
+    }
+    update(){
+        if(this.position.x > this.bounds.x){
+            this.position.x = 10;
+        }else if( this.position.x  <0){
+            this.position.x = this.bounds.x -10;
+        }
+        if(this.position.y > this.bounds.y){
+            this.position.y = 10;
+        }else if( this.position.y <0){
+            this.position.y = this.bounds.y -10;
+        }
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+    }
+    distanceTo(other:AnimatedPoint){
+        return this.position.distanceTo(other.position);
     }
 }
-
-
-function main(){
-
+class Rendering {
+    canvas: HTMLCanvasElement;
+    context: CanvasRenderingContext2D;
+    isRed: boolean;
+    colorString: string = 'rgb(200,0,0)';
+    pointsList = [];
+    seperationDistance:number = 90;
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        this.setBackgroundColor(this.colorString);
+        this.context.fillRect(0, 0, 800, 800);
+        this.isRed = true;
+        let bounds: Vector2 = new Vector2(this.canvas.width, this.canvas.height);
+        for(let x =0; x<100; x++){
+            let xPos = Math.random()*this.canvas.width +8;
+            let yPos = Math.random()*this.canvas.height +8;
+            
+            this.pointsList[x] = new AnimatedPoint(4,4,new Vector2(xPos,yPos),bounds);
+        }
+    }
+    setBackgroundColor(color:string){
+        this.context.fillStyle = color;
+        this.context.fillRect(0,0,800,800);
+    }
+    update() {
+        this.context.fillStyle = 'black';
+        this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+        for(let i =0; i<this.pointsList.length-1; i++){
+            this.pointsList[i].update();
+            this.context.beginPath();
+            this.context.moveTo(this.pointsList[i].position.x,this.pointsList[i].position.y);
+            this.context.arc(this.pointsList[i].position.x, this.pointsList[i].position.y, this.pointsList[i].width,0,360);
+            this.context.fill();
+            this.context.fill();
+            for(let x=i+1; x<this.pointsList.length-1; x++){
+                if(x != this.pointsList.length-1){
+                    if(this.pointsList[i].distanceTo(this.pointsList[x])<=this.seperationDistance){
+                        this.context.lineTo(this.pointsList[x].position.x,this.pointsList[x].position.y);
+                    }
+                }
+            }
+            this.context.stroke();
+            
+        }
+    }
 }
+function initialize() {
+    var render = new Rendering(document.getElementById("mainCanvas") as HTMLCanvasElement);
+    var timer = setInterval(()=>render.update(), 18);
+}
+
